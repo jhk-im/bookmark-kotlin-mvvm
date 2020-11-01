dfe8c77084c0c9d0714be0ca8b0ed9737277ee98
hun <wanted0301@gmail.com>
2020-11-01 20:43:17 +0900

    feature mvvm pattern to bookmarkdetail package

---
+++ app/src/main/java/com/example/bookmarkse_kotlin/bookmarkdetail/BookmarkDetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.bookmarkse_kotlin.bookmarkdetail
+
+import androidx.lifecycle.ViewModel
+import com.example.bookmarkse_kotlin.data.source.ItemsRepository
+
+class BookmarkDetailViewModel(
+    private val itemRepository: ItemsRepository
+) : ViewModel() {
+
+
+
+}
